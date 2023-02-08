86278e76fb0bfe6dd04872b6c05620b9c2ef0fa2
suyeon <develop.dduddu@gmail.com>
2023-02-08 16:48:44 +0900

    Initial Commit

---
+++ baguette/baguetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  baguetteApp.swift
+//  baguette
+//
+//  Created by suyeon park on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct baguetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
